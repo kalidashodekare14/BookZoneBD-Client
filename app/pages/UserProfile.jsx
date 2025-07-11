e89ff0a767156a35ebfbb7bacedc951db0afc679
Kalidash Odekare <shaktiadhikari49@gmail.com>
2025-07-11 10:36:49 +0600

    Profile page information update modal implement

--- app/pages/UserProfile.jsx
+++ app/pages/UserProfile.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { FaEdit, FaInfo, FaLock } from 'react-icons/fa';
 import { Modal } from 'react-responsive-modal';
 import 'react-responsive-modal/styles.css';
+import { FaCamera } from "react-icons/fa";
+
 
 const UserProfile = () => {
 
@@ -66,7 +68,58 @@ const UserProfile = () => {
                     </div>
                 </div>
                 <Modal open={open} onClose={onCloseModal} center>
-                    <h2>Simple centered modal</h2>
+                    <div className='flex justify-center items-center'>
+                        <div className="relative w-20 mb-5 rounded-full">
+                            <img
+                                alt="Tailwind CSS Navbar component"
+                                src="https://i.ibb.co/WcTWxsN/nav-img.png" />
+                            <div className='absolute bottom-0 right-0 bg-[#cfcfcf] p-2 rounded-full'>
+                                <FaCamera className=' text-xl' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='space-y-3'>
+                        <div className='flex flex-col w-full'>
+                            <label htmlFor="">Name</label>
+                            <input className='input focus:outline-0 w-full' type="text" />
+                        </div>
+                        <div className='flex items-center gap-3'>
+                            <div className='flex flex-col w-full'>
+                                <label htmlFor="">Date Of Birth</label>
+                                <input className='input focus:outline-0 w-full' type="date" />
+                            </div>
+                            <div className='flex flex-col w-full'>
+                                <label htmlFor="">Gender</label>
+                                <select className='border border-[#bbb] py-[6px] rounded-[5px]' name="" id="">
+                                    <option value="">Male</option>
+                                    <option value="">Female</option>
+                                    <option value="">Others</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className='flex flex-col w-full'>
+                            <label htmlFor="">Email</label>
+                            <input className='input focus:outline-0 w-full' type="email" />
+                        </div>
+                        <div className='flex flex-col w-full'>
+                            <label htmlFor="">Address</label>
+                            <input className='input focus:outline-0 w-full' type="text" />
+                        </div>
+                        <div className='flex flex-col w-full'>
+                            <label htmlFor="">Contact Number</label>
+                            <input className='input focus:outline-0 w-full' type="text" />
+                        </div>
+                        <div className='flex items-center gap-3'>
+                            <div className='flex flex-col w-full'>
+                                <label htmlFor="">City</label>
+                                <input className='input focus:outline-0 w-full' type="text" />
+                            </div>
+                            <div className='flex flex-col w-full'>
+                                <label htmlFor="">State</label>
+                                <input className='input focus:outline-0 w-full' type="text" />
+                            </div>
+                        </div>
+                    </div>
                 </Modal>
             </div>
         </div>
