42d03164cdd5b5b641b7efa824d7d689de551b3e
Kalidash Odekare <shaktiadhikari49@gmail.com>
2025-07-24 12:23:07 +0600

    CheckoutPayment page shipping data update

--- app/pages/Checkout.jsx
+++ app/pages/Checkout.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoIosCloseCircle } from 'react-icons/io';
 import { useCart } from 'react-use-cart';
-
+import 'react-responsive-modal/styles.css';
 import { Link } from 'react-router';
+import useUser from '../hooks/useUser';
+import { FaCheck, FaEdit } from 'react-icons/fa';
+import Modal from 'react-responsive-modal';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { profileDataUpdate } from '../Redux/slice/profileSlice';
+import useAuth from '../hooks/useAuth';
 
 const Checkout = () => {
+
+    const [userData, loading, error] = useUser();
+    const { user } = useAuth();
+    const [open, setOpen] = useState(false);
+    const onOpenModal = () => setOpen(true);
+    const onCloseModal = () => setOpen(false);
+    const dispatch = useDispatch();
+    const [isAddressText, setIsAddressText] = useState(userData?.address);
+    console.log('checking textarea', isAddressText)
+    const shippingInfo = userData?.name && userData?.email && userData?.country && userData?.address && userData?.contact_number && userData?.alternative_phone_number && userData?.state && userData?.city && userData?.union
+
 
     const salesTaxRate = 0.1
     const {
@@ -22,10 +40,35 @@ const Checkout = () => {
     }, 0)
 
     const salesTax = discountedCartTotal * salesTaxRate;
-    const grandTotal = discountedCartTotal + salesTax
+    const grandTotal = discountedCartTotal + salesTax;
 
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
 
+    const onSubmit = (data) => {
+        console.log(data)
+        if (isAddressText.length === 0) {
+            return console.log('address must required');
+        }
+        const shippingInfo = {
+            name: data.name,
+            contact_number: data.contact_number,
+            city: data.city,
+            state: data.state,
+            union: data.union,
+            address: isAddressText
+        }
 
+        console.log('checking datas', shippingInfo)
+        dispatch(profileDataUpdate({ email: user?.email, data: shippingInfo }))
+        if (loading === false) {
+            onCloseModal()
+        }
+    }
 
     return (
         <div className='mx-10 my-10 font-mixed min-h-[600px]'>
@@ -82,34 +125,85 @@ const Checkout = () => {
                         </tbody>
                     </table>
                 </div>
-                <div className='w-96 h-72 border border-[#bbb] p-3'>
-                    <div >
-                        <p>Enter Promo Code</p>
-                        <div className='flex items-center'>
-                            <input className='input focus:outline-0' type="text" />
-                            <button className='btn'>Submit</button>
+                <div className='w-96 space-y-2'>
+                    {
+                        shippingInfo && (
+                            <div className='border border-[#bbb] p-3'>
+                                <div className='flex justify-between items-center'>
+                                    <p className='text-xl mb-3'>Shipping Address</p>
+                                    <FaEdit onClick={onOpenModal} className='text-xl cursor-pointer' />
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <FaCheck className='text-[#22a733]' />
+                                    <h2>{userData?.state}</h2>
+                                </div>
+                                <p className='text-[#535353]'>{userData?.name}</p>
+                                <p className='text-[#535353]'>{userData?.contact_number}</p>
+                            </div>
+                        )
+                    }
+                    <div className='h-72 border border-[#bbb] p-3'>
+                        <div >
+                            <p className='text-xl mb-3'>Enter Promo Code</p>
+                            <div className='flex items-center'>
+                                <input className='input focus:outline-0' type="text" />
+                                <button className='btn'>Submit</button>
+                            </div>
                         </div>
-                    </div>
-                    <div className='space-y-2 mt-3'>
-                        <div className='flex justify-between items-center '>
-                            <p>Subtotal:</p>
-                            <p>৳{discountedCartTotal}</p>
+                        <div className='space-y-2 mt-3'>
+                            <div className='flex justify-between items-center '>
+                                <p>Subtotal:</p>
+                                <p>৳{discountedCartTotal}</p>
+                            </div>
+                            <div className='flex justify-between items-center '>
+                                <p>Sales Tax:</p>
+                                <p>৳{salesTax}</p>
+                            </div>
+                            <div className='flex justify-between items-center '>
+                                <p>Grand Total: </p>
+                                <p>৳{grandTotal}</p>
+                            </div>
                         </div>
-                        <div className='flex justify-between items-center '>
-                            <p>Sales Tax:</p>
-                            <p>৳{salesTax}</p>
+                        <div className='flex justify-center items-center mt-5'>
+                            <Link to={"/checkout_payment"} >
+                                <button className='btn w-52 bg-[#003a5a] text-white'>Proceed to checkout</button>
+                            </Link>
                         </div>
-                        <div className='flex justify-between items-center '>
-                            <p>Grand Total: </p>
-                            <p>৳{grandTotal}</p>
-                        </div>
-                    </div>
-                    <div className='flex justify-center items-center mt-5'>
-                        <Link to={"/checkout_payment"} >
-                            <button className='btn w-52 bg-[#003a5a] text-white'>Proceed to checkout</button>
-                        </Link>
                     </div>
                 </div>
+                <Modal open={open} onClose={onCloseModal} center>
+                    <form onSubmit={handleSubmit(onSubmit)} className='space-y-2'>
+                        <p className='mb-5'>Shopping Info</p>
+                        <div className='grid grid-cols-2 gap-2'>
+                            <div className='w-full'>
+                                <input {...register("name", { required: true })} className='input focus:outline-0 w-full' defaultValue={userData?.name} type="text" />
+                            </div>
+                            <div className='w-full'>
+                                <input {...register("contact_number", { required: true })} className='input focus:outline-0 w-full' defaultValue={userData?.contact_number} type="text" />
+                            </div>
+                            <div className='w-full'>
+                                <input {...register("city", { required: true })} className='input focus:outline-0 w-full' defaultValue={userData?.city} type="text" />
+                            </div>
+                            <div className='w-full'>
+                                <input {...register("state", { required: true })} className='input focus:outline-0 w-full' defaultValue={userData?.state} type="text" />
+                            </div>
+                        </div>
+                        <div className='w-full'>
+                            <input {...register("union", { required: true })} className='input focus:outline-0 w-full' defaultValue={userData?.union} type="text" />
+                        </div>
+                        <div className='w-full'>
+
+                            <textarea onChange={(e) => setIsAddressText(e.target.value)}
+                                defaultValue={userData?.address}
+                                className='textarea focus:outline-0 w-full'
+                                name=""
+                                id=""></textarea>
+                        </div>
+                        <div className='flex justify-center items-center'>
+                            <button type='submit' className='btn w-32 bg-[#003a5a] text-white'>Save</button>
+                        </div>
+                    </form>
+                </Modal>
             </div>
         </div>
     );
--- app/pages/CheckoutPayment.jsx
+++ app/pages/CheckoutPayment.jsx
@@ -5,6 +5,10 @@ import { useCart } from 'react-use-cart';
 import useUser from '../hooks/useUser';
 import { ThreeDot } from 'react-loading-indicators';
 import axiosSecure from '../utils/axiosSecure';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { profileDataUpdate } from '../Redux/slice/profileSlice';
+import useAuth from '../hooks/useAuth';
 
 const CheckoutPayment = () => {
 
@@ -13,7 +17,17 @@ const CheckoutPayment = () => {
     const salesTaxRate = 0.1
     const [isCashOnDalivery, setIsCashOnDalivery] = useState(false);
     const [isSslcommerz, setIsSslcommerz] = useState(false);
+    const shipppingAddress = !userData?.name || !userData?.email || !userData?.country || !userData?.address || !userData?.contact_number || !userData?.alternative_phone_number || !userData?.state || !userData?.city || !userData?.union
+    const [isAddressText, setIsAddressText] = useState(userData?.address);
+    const dispatch = useDispatch();
+    const { user } = useAuth();
 
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
 
     const {
         items,
@@ -73,6 +87,25 @@ const CheckoutPayment = () => {
         }
     }
 
+    const onSubmit = (data) => {
+        console.log(data)
+        if (isAddressText.length === 0) {
+            return console.log('address must required');
+        }
+        const shippingInfoData = {
+            name: data.name,
+            contact_number: data.contact_number,
+            alternative_phone_number: data.alternative_phone_number,
+            country: data.country,
+            city: data.city,
+            state: data.state,
+            union: data.union,
+            address: isAddressText
+        }
+        console.log('checking datas', shippingInfoData)
+        dispatch(profileDataUpdate({ email: user?.email, data: shippingInfoData }))
+    }
+
     const handleCashOnDalivery = () => {
         if (!isCashOnDalivery) {
             setIsCashOnDalivery(true);
@@ -90,46 +123,46 @@ const CheckoutPayment = () => {
     return (
         <div className='px-20 py-10 font-mixed min-h-[600px] bg-[#e4e4e4]'>
             <div className='flex gap-20'>
-                <div className='w-[65%] space-y-5'>
+                <form onSubmit={handleSubmit(onSubmit)} className='w-[65%] space-y-5'>
                     {/* Shipping Address */}
                     {
-                        (!userData?.name || !userData?.email || !userData?.country || !userData?.address || !userData?.contact_number || !userData?.alternative_phone_number || !userData?.state || !userData?.city || !userData?.union) && (
+                        shipppingAddress && (
                             <div className='bg-[#f8f8f8] p-5'>
                                 <h1 className='text-xl'>Shipping Address</h1>
                                 <div className='w-full'>
                                     <div className='flex flex-col w-full'>
                                         <label htmlFor="">Name</label>
-                                        <input className='input focus:outline-0 w-full' defaultValue={userData?.name} type="text" />
+                                        <input {...register("name")} className='input focus:outline-0 w-full' defaultValue={userData?.name} type="text" />
                                     </div>
                                     <div className='grid grid-cols-1 lg:grid-cols-2 gap-2 '>
                                         <div className='flex flex-col gap-1 w-full'>
                                             <label htmlFor="">Phone Number</label>
-                                            <input className='input focus:outline-0 w-full' defaultValue={userData?.contact_number} type="text" />
+                                            <input {...register("contact_number")} className='input focus:outline-0 w-full' defaultValue={userData?.contact_number} type="text" />
                                         </div>
                                         <div className='flex flex-col gap-1 w-full'>
                                             <label htmlFor="">Alternative Phone Number</label>
-                                            <input className='input focus:outline-0 w-full' defaultValue={userData?.alternative_phone_number} type="text" />
+                                            <input {...register("alternative_phone_number")} className='input focus:outline-0 w-full' defaultValue={userData?.alternative_phone_number} type="text" />
                                         </div>
                                         <div className='flex flex-col gap-1 w-full'>
                                             <label htmlFor="">Country</label>
-                                            <input className='input focus:outline-0 w-full' defaultValue={userData?.country} type="text" />
+                                            <input {...register("country")} className='input focus:outline-0 w-full' defaultValue={userData?.country} type="text" />
                                         </div>
                                         <div className='flex flex-col gap-1 w-full'>
                                             <label htmlFor="">City</label>
-                                            <input className='input focus:outline-0 w-full' defaultValue={userData?.city} type="text" />
+                                            <input {...register("city")} className='input focus:outline-0 w-full' defaultValue={userData?.city} type="text" />
                                         </div>
                                         <div className='flex flex-col gap-1 w-full'>
                                             <label htmlFor="">State</label>
-                                            <input className='input focus:outline-0 w-full' defaultValue={userData?.state} type="text" />
+                                            <input {...register("state")} className='input focus:outline-0 w-full' defaultValue={userData?.state} type="text" />
                                         </div>
                                         <div className='flex flex-col gap-1 w-full'>
                                             <label htmlFor="">Union</label>
-                                            <input className='input focus:outline-0 w-full' defaultValue={userData?.union} type="text" />
+                                            <input {...register("union")} className='input focus:outline-0 w-full' defaultValue={userData?.union} type="text" />
                                         </div>
                                     </div>
                                     <div>
                                         <label htmlFor="">Address</label>
-                                        <textarea className='textarea focus:outline-0 w-full h-32' defaultValue={userData?.address} placeholder='বাসা/ফ্ল্যাট, পাড়া-মহল্লার নাম, পরিচিতি এলাকা উল্লেখ করুন' name="" id=""></textarea>
+                                        <textarea onChange={(e) => setIsAddressText(e.target.value)} className='textarea focus:outline-0 w-full h-32' defaultValue={userData?.address} placeholder='বাসা/ফ্ল্যাট, পাড়া-মহল্লার নাম, পরিচিতি এলাকা উল্লেখ করুন' name="" id=""></textarea>
                                     </div>
                                 </div>
                             </div>
@@ -163,11 +196,11 @@ const CheckoutPayment = () => {
                         </div>
                         <div className='flex justify-center items-center my-5'>
                             {
-                                isCashOnDalivery && <button className='btn w-40 p-5 bg-[#003a5a] text-white'>Confirm Order</button>
+                                isCashOnDalivery && <button type={shipppingAddress ? "submit" : "button"} className='btn w-40 p-5 bg-[#003a5a] text-white'>Confirm Order</button>
                             }
                             {
                                 isSslcommerz && (
-                                    <button onClick={handlePaymentSystem} className='btn w-40 p-5 bg-[#003a5a] text-white'>
+                                    <button type={shipppingAddress ? "submit" : "button"} onClick={handlePaymentSystem} className='btn w-40 p-5 bg-[#003a5a] text-white'>
                                         {
                                             paymentLoading ? <ThreeDot color="#ffffff" size="small" text="" textColor="" /> : "Confirm Order"
                                         }
@@ -176,7 +209,7 @@ const CheckoutPayment = () => {
                             }
                         </div>
                     </div>
-                </div>
+                </form>
                 <div className='right-20 w-96 h-52 border border-[#bbb] p-3 bg-[#f8f8f8] '>
                     <div >
                         <p>Enter Promo Code</p>
