e132b11d609ec5761929cb35fe47a2f36c394477
Kalidash Odekare <shaktiadhikari49@gmail.com>
2025-08-15 13:42:48 +0600

    Publisher data update and delete system implement

--- app/Redux/slice/dashboardSlice/publishersManageSlice.js
+++ app/Redux/slice/dashboardSlice/publishersManageSlice.js
@@ -10,6 +10,23 @@ export const publisherManageFatched = createAsyncThunk(
     }
 )
 
+export const publisherDataUpdate = createAsyncThunk(
+    "totalPublisher/publisherDataUpdate",
+    async ({ id, data }) => {
+        const res = await axiosSecure.patch(`/api/dashboard/publisher_info_update/${id}`, data);
+        return res.data.data
+    }
+)
+
+
+export const publisherDataDelete = createAsyncThunk(
+    "totalPublisher/publisherDataDelete",
+    async ({ id }) => {
+        const res = await axiosSecure.delete(`/api/dashboard/publisher_delete/${id}`);
+        return res.data.data
+    }
+)
+
 
 const dashboardAuthorsManage = createSlice({
     name: "allPublishers",
@@ -26,11 +43,49 @@ const dashboardAuthorsManage = createSlice({
                 state.error = null;
             })
             .addCase(publisherManageFatched.fulfilled, (state, action) => {
-                state.loading = false;
                 state.totalpublisher = action.payload.books;
                 state.totalPages = action.payload.totalPages;
+                state.loading = false;
             })
             .addCase(publisherManageFatched.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+
+
+            // Publisher data update
+            .addCase(publisherDataUpdate.pending, (state, action) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(publisherDataUpdate.fulfilled, (state, action) => {
+                const publisherData = action.payload
+                const index = state.totalpublisher.findIndex(publisher => publisher._id === publisherData._id);
+                if (index !== -1) {
+                    state.totalpublisher[index] = { ...state.totalpublisher[index], ...publisherData }
+                }
+                state.loading = false;
+            })
+            .addCase(publisherDataUpdate.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+
+
+            // Publisher data delete
+            .addCase(publisherDataDelete.pending, (state, action) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(publisherDataDelete.fulfilled, (state, action) => {
+                const publisherData = action.payload
+                const index = state.totalpublisher.findIndex(publisher => publisher._id === publisherData._id);
+                if (index !== -1) {
+                    state.totalpublisher.splice(index, 1);
+                }
+                state.loading = false;
+            })
+            .addCase(publisherDataDelete.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.payload;
             })
--- app/pages/Dashboard/AdminPage/PublisherManage.jsx
+++ app/pages/Dashboard/AdminPage/PublisherManage.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { publisherManageFatched } from '../../../Redux/slice/dashboardSlice/publishersManageSlice';
+import { publisherManageFatched, publisherDataUpdate, publisherDataDelete } from '../../../Redux/slice/dashboardSlice/publishersManageSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import ReactPaginate from 'react-paginate';
 import { OrbitProgress } from 'react-loading-indicators';
 import { CiSearch } from 'react-icons/ci';
 import { HiDotsVertical } from 'react-icons/hi';
 import { Link } from 'react-router';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
+import { FaCamera } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+import Swal from 'sweetalert2';
+
+const IMG_API_KEY = import.meta.env.VITE_IMG_API_KEY;
+const IMG_HOSTING = `https://api.imgbb.com/1/upload?key=${IMG_API_KEY}`
 
 const AuthorManage = () => {
 
@@ -14,9 +22,25 @@ const AuthorManage = () => {
     const [currentPage, setCurrentPage] = useState(0);
     const limit = 10;
     const [isSearch, setIsSearch] = useState(null);
+    const [open, setOpen] = useState(false);
+    const onOpenModal = () => setOpen(true);
+    const onCloseModal = () => setOpen(false);
+    const [imageHosting, setImageHosting] = useState("");
+    const [imgHostingLoading, setImgHostingLoading] = useState(false);
+    const [selectedPublisher, setSelectedPublisher] = useState(null);
+    const [publisherBio, setPublisherBio] = useState("")
 
 
-    console.log('checking publisher', totalpublisher)
+
+    useEffect(() => {
+        setImageHosting(selectedPublisher?.publisher_image)
+    }, [selectedPublisher?.publisher_image])
+
+    useEffect(() => {
+        setPublisherBio(selectedPublisher?.publisher_description)
+    }, [selectedPublisher?.publisher_description])
+
+
 
     useEffect(() => {
         const params = new URLSearchParams({
@@ -37,12 +61,94 @@ const AuthorManage = () => {
         setCurrentPage(data.selected)
     }
 
-    if (loading) {
-        return <div className='h-[550px] flex flex-col justify-center items-center'>
-            <OrbitProgress variant="spokes" color="#003a5a" size="large" text="" textColor="" />
-            <p className='text-xl'>Please wait...</p>
-        </div>
+    const handleImageHosting = async (event) => {
+        const imageSelected = event.target.files[0];
+        setImgHostingLoading(true)
+        const formData = new FormData()
+        formData.append("image", imageSelected);
+        try {
+            const res = await fetch(`${IMG_HOSTING}`, {
+                method: "POST",
+                body: formData
+            })
+            const data = await res.json();
+            if (data.success) {
+                setImageHosting(data.data.url);
+            }
+        } catch (error) {
+            console.log(error.message);
+        } finally {
+            setImgHostingLoading(false)
+        }
     }
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+        try {
+            console.log(data)
+            if (!publisherBio) {
+                return
+            }
+            const publisherInfo = {
+                publisher_name: data.publisher_name,
+                publisher_website: data.publisher_website,
+                publisher_description: publisherBio
+            }
+            dispatch(publisherDataUpdate({ id: selectedPublisher._id, data: publisherInfo }));
+            if (!loading) {
+                onCloseModal()
+            }
+        } catch (error) {
+            console.error(error.message);
+        }
+
+    }
+
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    dispatch(publisherDataDelete({ id: id }))
+                    if (!loading) {
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your publish data has been deleted.",
+                            icon: "success"
+                        });
+                    }
+                } catch (error) {
+                    console.error(error.message);
+                }
+
+            }
+        });
+
+    }
+
+
+
+
+    // if (loading) {
+    //     return <div className='h-[550px] flex flex-col justify-center items-center'>
+    //         <OrbitProgress variant="spokes" color="#003a5a" size="large" text="" textColor="" />
+    //         <p className='text-xl'>Please wait...</p>
+    //     </div>
+    // }
 
 
     return (
@@ -70,25 +176,25 @@ const AuthorManage = () => {
                     <tbody>
                         {
                             totalpublisher.length > 0 ? (
-                                totalpublisher.map(author => (
+                                totalpublisher.map(publisher => (
                                     <tr className='text-[15px]'>
                                         <th>
-                                            <img className='w-14 h-14 rounded-full' src={author?.publisher_image ? author.publisher_image : "https://i.ibb.co/WcTWxsN/nav-img.png"} alt="" />
+                                            <img className='w-14 h-14 rounded-full' src={publisher?.publisher_image ? publisher.publisher_image : "https://i.ibb.co/WcTWxsN/nav-img.png"} alt="" />
                                         </th>
-                                        <td>{author?.publisher_name ? author?.publisher_name : "N/A"}</td>
+                                        <td>{publisher?.publisher_name ? publisher?.publisher_name : "N/A"}</td>
                                         <td>
-                                            <Link className='text-[#3bb77c]' to={author?.publisher_website}>
-                                                {author?.publisher_website ? author?.publisher_website : "N/A"}
+                                            <Link className='text-[#3bb77c]' to={publisher?.publisher_website} target='_blank'>
+                                                {publisher?.publisher_website ? publisher?.publisher_website : "N/A"}
                                             </Link>
 
                                         </td>
-                                        <td>{author?.publisher_description ? author?.publisher_description.slice(0, 40) : "N/A"} {author?.publisher_description.length > 40 && "..."}</td>
+                                        <td>{publisher?.publisher_description ? publisher?.publisher_description.slice(0, 40) : "N/A"} {publisher?.publisher_description.length > 40 && "..."}</td>
                                         <td>
                                             <div className="dropdown dropdown-bottom dropdown-end">
                                                 <div tabIndex={0} role="button" className="btn m-1"><HiDotsVertical /></div>
-                                                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                                                    <li>Edit</li>
-                                                    <li className='bg-[#d00000] text-white'>Delete</li>
+                                                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm space-y-2">
+                                                    <li onClick={() => { onOpenModal(), setSelectedPublisher(publisher) }} className='bg-[#0081a7] text-white p-2 cursor-pointer'>Edit</li>
+                                                    <li onClick={() => handleDelete(publisher._id)} className='bg-[#d00000] text-white p-2 cursor-pointer'>Delete</li>
                                                 </ul>
                                             </div>
                                         </td>
@@ -108,6 +214,46 @@ const AuthorManage = () => {
                     </tbody>
                 </table>
             </div>
+            <Modal open={open} onClose={onCloseModal} center>
+                <div>
+                    <div className='flex justify-center items-center'>
+                        <div className="relative w-28 h-28 mb-5 rounded-full">
+                            <img
+                                className='w-full h-full rounded-full'
+                                src={imageHosting ? imageHosting : "https://i.ibb.co/WcTWxsN/nav-img.png"}
+                                alt={"image"}
+                            />
+
+                            <div onClick={() => document.querySelector('input[type="file"]').click()} className={`absolute cursor-pointer bottom-0 right-0 ${imgHostingLoading ? "bg-[#39b9ca]" : "bg-[#cfcfcf]"}  p-2 w-10 h-10 flex justify-center items-center rounded-full`}>
+                                {
+                                    imgHostingLoading ? <span class="loader"></span> : <FaCamera className=' text-xl' />
+                                }
+
+                                <input onChange={handleImageHosting} hidden type="file" />
+                            </div>
+                        </div>
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className='flex flex-col lg:flex-row items-center gap-4'>
+                            <div>
+                                <label htmlFor="">Name</label>
+                                <input {...register("publisher_name", { required: true })} className='input focus:outline-0 w-full' defaultValue={selectedPublisher?.publisher_name} placeholder='Edit name' type="text" />
+                            </div>
+                            <div>
+                                <label htmlFor="">Website</label>
+                                <input {...register("publisher_website", { required: true })} className='input focus:outline-0 w-full' defaultValue={selectedPublisher?.publisher_website} placeholder='Edit website' type="text" />
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor="">Bio</label>
+                            <textarea onChange={(event) => setPublisherBio(event.target.value)} value={publisherBio} className='textarea focus:outline-0 w-full h-32' placeholder='Edit website' type="text" />
+                        </div>
+                        <div className='flex justify-center items-center mt-5'>
+                            <button className='btn bg-[#003a5a] text-white'>Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </Modal>
             <div className='my-10 flex justify-center items-center'>
                 {
                     totalpublisher.length > 0 && (
